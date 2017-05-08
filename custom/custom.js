53852360b6b0b3d2ff76506994a25ad50e04efd4
WaQaR Ali <proudwaqar@gmail.com>
2017-05-08 12:20:51 +0500

    Add files via upload

---
+++ custom/custom.js
@@ -0,0 +1,31 @@
+// Full list of configuration options available at:
+// https://github.com/hakimel/reveal.js#configuration
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+    transition: 'slide', // none/fade/slide/convex/concave/zoom
+    //parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
+    parallaxBackgroundImage: 'custom/reveal-parallax-1.jpg',
+
+    // Optional reveal.js plugins
+    dependencies: [
+        {src: 'lib/js/classList.js', condition: function() {
+                return !document.body.classList;
+            }},
+        {src: 'plugin/markdown/marked.js', condition: function() {
+                return !!document.querySelector('[data-markdown]');
+            }},
+        {src: 'plugin/markdown/markdown.js', condition: function() {
+                return !!document.querySelector('[data-markdown]');
+            }},
+        {src: 'plugin/highlight/highlight.js', async: true, condition: function() {
+                return !!document.querySelector('pre code');
+            }, callback: function() {
+                hljs.initHighlightingOnLoad();
+            }},
+        {src: 'plugin/zoom-js/zoom.js', async: true},
+        {src: 'plugin/notes/notes.js', async: true}
+    ]
+});
